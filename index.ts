615a524bbe5bed490c28d9e8e23e83f031328e70
Schmalzl Márton <marton.schmalzl@gmail.com>
2023-11-20 14:32:33 +0100

    init

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+console.log(process.env);
+const port = parseInt(process.env.PORT||'') || 8080;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
